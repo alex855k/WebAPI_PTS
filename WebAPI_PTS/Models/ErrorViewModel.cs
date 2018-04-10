695dfed3a4019f053ce6292df7e74436e680b1df
Alexander Oliver Nymann Hvidt <alex855k@edu.eal.dk>
2018-04-10 10:59:24 +0200

    Add project files.

---
+++ WebAPI_PTS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAPI_PTS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
