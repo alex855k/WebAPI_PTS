dce7b2c3b4dbb2ec1f5494bbb8c48830004bb2cf
Alexander Oliver Nymann Hvidt <alex855k@edu.eal.dk>
2018-04-10 19:48:02 +0200

    Added models, with the correct datatype need to specific some relations ie weak entities - pictures, results-
     with data annotations or fluent API (OnModelCreate).

---
+++ WebAPI_PTS/Models/Models.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+namespace WebAPI_PTS.Models
+{
+    public class TrialsContext : DbContext
+    {
+        protected TrialsContext(DbContextOptions<TrialsContext> options) : base(options)
+        {
+        }
+        /*
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }*/
+
+        public DbSet<FieldBlock> FieldBlocks { get; set; }
+        public DbSet<SubBlock> SubBlocks { get; set; }
+        public DbSet<ProductCategory> ProductCategory { get; set; }
+        public DbSet<TrialType> TrialType { get; set; }
+        public DbSet<Crop> Crops { get; set; }
+        public DbSet<CropCategory> CropCategories { get; set;  }
+        public DbSet<Treatment> Treatment { get; set; }
+        public DbSet<TreatmentType> TreatmentTypes { get; set; }
+        public DbSet<ResultEntry> ResultVariableType { get; set; }
+        public DbSet<ResultType> ResultTypes { get; set; }
+        public DbSet<UnitType> UnitTypes { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        
+    }
+    public class FieldBlock {
+        public int FieldBlockId { get; set; }
+        public string BlockChar { get; set; }
+        public string Description { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+
+    public class SubBlock
+    {
+        public int SubBlockId { get; set; }       
+        public int TrialType { get; set; }
+        public string TrialDescription { get; set; }
+        public Comment SubBlockComment { get; set; }
+
+        public int FieldBlockId { get; set; }
+        public FieldBlock FieldBlock { get; set; }
+    }
+
+
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductOwner { get; set; }
+        public int ProductCategoryId { get; set; }
+    }
+
+    public class ProductCategory
+    {
+        public int ProductCategoryId { get; set; }
+        public string ProductCategoryName { get; set; }
+    }
+
+    public class TrialType
+    {
+        public int TrialTypeId { get; set; }
+        public string TrialTypeName { get; set; }
+    }
+
+    public class TrialGroup
+    {
+        public int FieldBlockId { get; set; }
+    }
+
+    public class Crop
+    {
+        public int CropId { get; set; }
+        public int CropName { get; set; }
+        public int CropCategoryId { get; set; }
+    }
+
+
+    public class CropCategory
+    {
+        public int CropCategoryId { get; set; }
+        public int CropCategoryName { get; set; }
+    }
+
+    public class Treatment
+    {
+        public int TreatmentId { get; set; }
+        public int TrialGroupId { get; set; }
+        public int TreatmentTypeId { get; set; }
+        public DateTime TreatmentDate { get; set; }
+        public string TreatmentStage { get; set; }
+        public List<TreamentProduct> ProductDosages { get; set; }
+    }
+    public class TreamentProduct
+    {
+        public int TreatmentProductId { get; set; }
+        public int TreatmentId { get; set; }
+        public int ProductId { get; set; }
+        
+    }
+
+    public class TreatmentType
+    {
+        public int TreatmentTypeId { get; set; }
+        public string TreatmentTypeName { get; set; }
+    }
+
+
+    public class ResultEntry
+    {
+        public int TreatmentProductId { get; set; }
+    }
+    public class ResultType
+    {
+        public int ResultTypeId { get; set; }
+    }
+
+    public class ResultTypeTreatmentProduct
+    {
+        public int ResultTypeId { get; set; }
+    }
+    public class UnitType
+    {
+        public int UnitTypeId { get; set; }
+        public string UnitTypeName { get; set; }
+    }
+    public class TreatmentComment
+    {
+        public int TreatmentCommentId { get; set; }
+
+        public int TreatmentId { get; set; }
+        public Treatment Treatment { get; set; }
+
+        public int CommentId { get; set; }
+        public Comment Comment { get; set; }
+    }
+
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public DateTime PostDate { get; set; }
+        public DateTime EditDate { get; set; }
+    }
+
+
+    public class TreatmentImage
+    {
+        public int ImageId { get; set; }
+        public byte[] ImageArr { get; set; }
+        public string Caption { get; set; }
+
+        public int TreatmentId { get; set; }
+    }
+}
--- WebAPI_PTS/Startup.cs
+++ WebAPI_PTS/Startup.cs
@@ -22,6 +22,9 @@ namespace WebAPI_PTS
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            ;
+           // var connection = @"Server=ealdb1.eal.local;Database=EJL83_DB;UserID=ejl83_usr;Password=Baz1nga83;Trusted_Connection=True;ConnectRetryCount=0";
+            //services.AddDbContext<ResultsContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
